7b8479f29dfc307f9f9ef1c68879afa127dd90fc
Ron Gierlach <rongierlach@gmail.com>
2017-09-14 17:34:36 -0500

    add share meta copy

---
+++ src/copy/share-meta.js
@@ -0,0 +1,16 @@
+export default {
+  title: 'SoundCoin.io',
+  description: 'The self-sustaining platform for independent audio.',
+
+  /* https://developers.facebook.com/docs/sharing/webmasters */
+  facebook: {
+    url: 'https://SoundCoin.io'
+    // image: require('../img/logos/fb-img.jpg')
+  },
+
+  /* https://dev.twitter.com/cards/markup */
+  twitter: {
+    card: 'summary',
+    site: '@SoundCoin_io'
+  }
+}
