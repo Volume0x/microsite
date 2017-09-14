1aadf28565d65f7c3c8cd1c5decf277a18ac1921
Ron Gierlach <rongierlach@gmail.com>
2017-09-14 16:28:57 -0500

    update layout to use scrollbar and header / footer components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,64 +1,21 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import Scrollbar from 'react-perfect-scrollbar'
 
-import './index.css'
+import 'react-perfect-scrollbar/dist/css/styles.css'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const Layout = ({ location: { pathname }, children }) => (
+  <Scrollbar>
+    <div className='flex flex-column flex-auto min-vh-100'>
+      <Header pathname={pathname} />
+      <main className='flex-auto'>
+        { children() }
+      </main>
+      <Footer />
     </div>
-  </div>
+  </Scrollbar>
 )
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
-
-export default TemplateWrapper
+export default Layout
