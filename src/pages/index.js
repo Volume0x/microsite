c1d4e14101960a2c741c4bda8e43a59b192480fd
Ron Gierlach <rongierlach@gmail.com>
2017-09-22 14:10:12 -0500

    break hero out of sections for layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import sections from '../content/sections'
+import Hero from '../components/Hero'
+import Section from '../components/Section'
+
 const IndexPage = () => (
-  <div />
+  <div>
+    <Hero />
+    {sections.map((props, i) =>
+      <Section key={i} {...props} />
+    )}
+  </div>
 )
 
 export default IndexPage
