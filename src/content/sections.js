9d6cf1d96b16a63c1cff9d0923be843ed7a3e61b
Ron Gierlach <rongierlach@gmail.com>
2017-09-22 13:38:24 -0500

    stub some sections content

---
+++ src/content/sections.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    id: 'platform',
+    title: 'Platform'
+    // children: require()
+  },
+  // {
+  //   id: 'whitepaper',
+  //   title: 'Whitepaper'
+  // },
+  {
+    id: 'token',
+    title: 'Token'
+  },
+  {
+    id: 'roadmap',
+    title: 'Roadmap'
+
+  },
+  {
+    id: 'team',
+    title: 'Team'
+  }
+]
