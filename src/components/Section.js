8e9f90d91a15eb2f7513efd5dc0e82b4d7926c49
Ron Gierlach <rongierlach@gmail.com>
2017-10-02 02:21:20 -0500

    more component changes

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import Waypoint from 'react-waypoint'
+import { Element } from 'react-scroll'
+import { navigateTo } from 'gatsby-link'
 
-const Section = ({id, className, children}) => (
-  <section id={id} className={`
-    ${className}
-    section
-    vh-50
-    pv4
-    ph4 ph5-m ph6-l
-  `}>
-    {children}
-  </section>
+const Section = ({id, name, className, pushRoute, children}) => (
+  <Element
+    id={name}
+    name={name}
+    className={`section vh-100 pv4 ph4 ph5-m ph6-l ${className || ''}`}
+  >
+    <Waypoint
+      bottomOffset={'4rem'}
+      onEnter={() => navigateTo(`#${name}`)}
+    >
+      <div className='h-100 v-mid'>
+        {children}
+      </div>
+    </Waypoint>
+  </Element>
 )
 
 export default Section
