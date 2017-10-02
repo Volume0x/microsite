cd41ee7d9d0f448e424a9ee50be9714a1d9dd0e5
Ron Gierlach <rongierlach@gmail.com>
2017-10-02 02:22:58 -0500

    simplify layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,11 +4,12 @@ import Scrollbar from 'react-perfect-scrollbar'
 import '../styles/index.scss'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import sections from '../content/sections'
 
-const Layout = ({ location: { pathname }, children }) => (
+const Layout = ({ children, ...props }) => (
   <Scrollbar>
     <div className='flex flex-column flex-auto min-vh-100'>
-      <Header pathname={pathname} />
+      <Header sections={sections} />
       <main className='flex-auto'>
         { children() }
       </main>
