69782017c9e33a31f79db0089edc994dd23e6217
Ron Gierlach <rongierlach@gmail.com>
2017-10-05 12:31:56 -0500

    make and use Logo component

--- src/components/Header/LogoLink.js
+++ src/components/Header/LogoLink.js
@@ -1,10 +1,7 @@
 import React from 'react'
 
+import Logo from '../Logo'
 import ScrollLink from '../ScrollLink'
-import LogoWhite from '../../img/logos/soundcoin-white.svg'
-import LogoColor from '../../img/logos/soundcoin-color.svg'
-import LogoIconWhite from '../../img/logos/soundcoin-icon-white.svg'
-import LogoIconColor from '../../img/logos/soundcoin-icon-color.svg'
 
 const LogoLink = ({negative, ...props}) => (
   <ScrollLink
@@ -12,8 +9,15 @@ const LogoLink = ({negative, ...props}) => (
     to={'home'}
     className='tl white flex items-center w4 w3-m w5-l h3'
   >
-    <img className='db dn-m db-l ' src={negative ? LogoColor : LogoWhite} />
-    <img className='dn db-m h3' src={negative ? LogoIconColor : LogoIconWhite} />
+    <Logo
+      className={`dn db-m h3 ${negative ? 'blue' : 'white'}`}
+      withGradient={negative}
+    />
+    <Logo
+      className={`db dn-m db-l ${negative ? 'blue' : 'white'}`}
+      withGradient={negative}
+      withText
+    />
   </ScrollLink>
 )
 
---
+++ src/components/Logo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import LogoIcon from '../img/logos/soundcoin-icon-white.svg'
+import LogoIconWithGradient from '../img/logos/soundcoin-icon-color.svg'
+import LogoWithText from '../img/logos/soundcoin-white.svg'
+import LogoWithGradientWithText from '../img/logos/soundcoin-color.svg'
+
+const selectSvg = (withText, withGradient) => {
+  if (withText) return withGradient
+    ? LogoWithGradientWithText
+    : LogoWithText
+  else return withGradient
+    ? LogoIconWithGradient
+    : LogoIcon
+}
+
+const Logo = ({ withText, withGradient, ...props }) => (
+  <img
+    {...props}
+    src={selectSvg(withText, withGradient)}
+  />
+)
+
+export default Logo
