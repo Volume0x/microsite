7d65fb6a481c20ed69ef40461d6e9ab2e35a7d5d
Ron Gierlach <rongierlach@gmail.com>
2017-10-05 17:59:31 -0500

    dispatch window events from layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,18 +1,63 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import throttle from 'lodash.throttle'
+import { bindActionCreators } from 'redux'
 
 import '../styles/index.scss'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import sections from '../content/sections'
+import { didScroll, didResize } from '../store/constants/actionCreators'
 
-const Layout = ({ children, ...props }) => (
-  <div className='flex flex-column flex-auto min-vh-100'>
-    <Header sections={sections} />
-    <main className='flex-auto'>
-      { children() }
-    </main>
-    <Footer />
-  </div>
-)
+class Layout extends React.Component {
+  constructor (props, context) {
+    super(props, context)
+    this.handleScroll = throttle(this.handleScroll.bind(this), 50)
+    this.handleResize = throttle(this.handleResize.bind(this), 250)
+  }
 
-export default Layout
+  componentDidMount () {
+    this.handleScroll()
+    this.handleResize()
+    window.addEventListener('scroll', this.handleScroll)
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.handleScroll)
+    window.removeEventListener('resize', this.handleResize)
+  }
+
+  handleScroll () {
+    // window.requestAnimationFrame(this.handleScroll)
+    this.props.didScroll()
+  }
+
+  handleResize () {
+    // window.requestAnimationFrame(this.handleResize)
+    this.props.didResize()
+  }
+
+  render () {
+    const { children } = this.props
+    return (
+      <div className='flex flex-column flex-auto min-vh-100'>
+        <Header sections={sections} />
+        <main className='flex-auto'>
+          { children() }
+        </main>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  didScroll: bindActionCreators(didScroll, dispatch),
+  didResize: bindActionCreators(didResize, dispatch)
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Layout)
