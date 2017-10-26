111a7bb39f686c659f352a66652882432750e388
Ron Gierlach <rongierlach@gmail.com>
2017-10-26 12:06:07 -0500

    lint

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Headline = ({ className, ... props }) => (
+const Headline = ({ className, ...props }) => (
   <h1
     className={`f1 f-subheadline-m f-headline-l pt5 pt6-l mt0 ${className || ' '}`}
     {...props}
