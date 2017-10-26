500492da62f91c7029ca7b78768b3210d6cd2394
Ron Gierlach <rongierlach@gmail.com>
2017-10-26 12:09:34 -0500

    new sections structure

--- src/content/sections.js
+++ src/content/sections.js
@@ -1,23 +1,38 @@
 export default [
   {
+    id: 'mission',
+    name: 'mission',
+    title: 'Mission',
+    component: require('../components/Mission')
+  },
+  {
+    id: 'platform',
     name: 'platform',
-    title: 'Platform'
-    // children: require()
-  },
-  // {
-  //   name: 'whitepaper',
-  //   title: 'Whitepaper'
-  // },
-  {
-    name: 'token',
-    title: 'Token'
-  },
-  {
-    name: 'roadmap',
-    title: 'Roadmap'
-  },
-  {
-    name: 'team',
-    title: 'Team'
+    title: 'Platform',
+    component: require('../components/Platform')
   }
+  // {
+  // id: 'whitepaper',
+  //   name: 'whitepaper',
+  //   title: 'Whitepaper',
+  // component: require('../components/Whitepaper')
+  // },
+  // {
+  //   id: 'token',
+  //   name: 'token',
+  //   title: 'Token',
+  //   component: require('../components/Token')
+  // },
+  // {
+  //   id: 'roadmap',
+  //   name: 'roadmap',
+  //   title: 'Roadmap',
+  //   component: require('../components/Roadmap')
+  // },
+  // {
+  //   id: 'team',
+  //   name: 'team',
+  //   title: 'Team',
+  //   component: require('../components/Team')
+  // }
 ]
