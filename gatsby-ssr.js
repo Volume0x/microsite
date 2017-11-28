89c6917e7d5bf107db60240e521e8ceccbcd96f3
Ron Gierlach <rongierlach@gmail.com>
2017-11-28 17:05:32 -0600

    update gatsby-ssr for prod

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,15 @@
-import { compose } from 'redux'
+import React from 'react'
 import { Provider } from 'react-redux'
 import { renderToString } from 'react-dom/server'
 
 import createStore from './src/store'
 
 exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => (
-  compose(
-    replaceBodyHTMLString,
-    renderToString,
-    Provider
-  )({
-    store: createStore(),
-    children: bodyComponent
-  })
+  replaceBodyHTMLString(
+    renderToString(
+      <Provider store={createStore()}>
+        { bodyComponent }
+      </Provider>
+    )
+  )
 )
