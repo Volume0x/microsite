6409db5c2c91273f791fc66d0210ed0d1d10e46a
Ron Gierlach <rongierlach@gmail.com>
2017-12-05 13:19:09 -0600

    add mail link to footer

--- src/components/Footer/Socials.js
+++ src/components/Footer/Socials.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import { Github, Twitter, Slack } from 'react-feather'
+import { Github, Twitter, Slack, Mail } from 'react-feather'
 
 const iconList = [
   ['slack', Slack, 'https://slack.volume0x.com'],
   ['github', Github, 'https://github.com/volume0x'],
-  ['twitter', Twitter, 'https://twitter.com/volume0x']
+  ['twitter', Twitter, 'https://twitter.com/volume0x'],
+  ['mail', Mail, 'mailto:contact@volume0x.com']
 ]
 
 const Socials = () => (
   <ul className='list order-0 order-1-ns ma0 pl0 pb2 pa0-ns flex flex-row justify-between justify-end-ns items-center'>
     { iconList.map(([key, Cmp, href]) => (
       <li key={key} className='ma2 ml3-ns mr0-ns'>
-        <a className='link blue dim' href={href} target='_blank'>
+        <a
+          href={href}
+          target={key !== 'mail' ? '_blank' : ''}
+          className='link blue dim'
+        >
           <Cmp />
         </a>
       </li>
